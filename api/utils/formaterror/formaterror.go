cfca97736d61a56e68fec0e2ab647ddb5523a52e
ilhmrrm <ilham26ramadhan@gmail.com>
2023-05-18 17:49:52 +0700

    continue tomorrow

---
+++ api/utils/formaterror/formaterror.go
@@ -0,0 +1,26 @@
+package formatter
+
+import (
+	"errors"
+	"strings"
+)
+
+func FormatError(err string) error {
+	if strings.Contains(err, "nickname") {
+		return errors.New("Nickname Alreadey Taken")
+	}
+
+	if strings.Contains(err, "email") {
+		return errors.New("Email Alreadey Taken")
+	}
+
+	if strings.Contains(err, "title") {
+		return errors.New("Title Alreadey Taken")
+	}
+
+	if strings.Contains(err, "hashedPassword") {
+		return errors.New("Incorrect Password")
+	}
+
+	return errors.New("Incorrect Details")
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/ilhmrrm/fullstack
+
+go 1.19
+
+require (
+	github.com/badoux/checkmail v1.2.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+)
